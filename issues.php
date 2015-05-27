6d1db9268373cad03dfca623f614ec17ba109c42
Tobias Tiederle <ttiederle@fimltd.org>
2015-05-27 18:02:15 +0100

    add issue management

---
+++ issues.php
@@ -0,0 +1,100 @@
+<?php
+require_once 'includes/bootstrap.php';
+
+function _requestOrDefault($parameter, $default = '', $null_value = null) {
+	$value = (isset($_REQUEST[$parameter])) ? $_REQUEST[$parameter] : $default;
+	if ($value === $null_value) {
+		$value = null;
+	}
+	return $value;
+}
+
+$issue_id = isset($_REQUEST['id']) ? $_REQUEST['id'] : NEW_ENTRY_ID;
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : 'list';
+$page_url = strtok($_SERVER["REQUEST_URI"], '?');
+
+// new issue?
+$new_issue = false;
+if ($issue_id == NEW_ENTRY_ID) {
+	$new_issue = true;
+}
+
+switch ($action) {
+	case 'delete':
+		$query = "DELETE FROM `%table` WHERE IssueId = :id";
+		$values = array(
+			'%table' => TABLE_ISSUES,
+			':id' => $issue_id,
+		);
+		$stmt = db()->preparedStatement($query, $values);
+		if (!$stmt->success) {
+			die('Database delete fail: ' . $stmt->error);
+		}
+		header('Location: ' . $page_url . '?action=list', true, 302);
+		break;
+	case 'save':
+		// get values for issue table
+		$values = array(
+			'%table' => TABLE_ISSUES,
+			':title' => _requestOrDefault('IssueTitle'),
+			':description' => _requestOrDefault('IssueDescription'),
+		);
+		if ($new_issue) {
+			$query = "INSERT INTO `%table` SET Title = :title, Description = :description";
+		} else {
+			$query = "UPDATE `%table` SET Title = :title, Description = :description WHERE IssueId = :id";
+			$values[':id'] = $issue_id;
+		}
+		$stmt = db()->preparedStatement($query, $values);
+		if (!$stmt->success) {
+			die('Database update fail: ' . $stmt->error);
+		} elseif ($new_issue) {
+			$issue_id = $stmt->lastInsertId;
+		}
+		header('Location: ' . $page_url . '?action=list', true, 302);
+		break;
+	case 'edit':
+			if ($new_issue) {
+				$title = 'New Issue';
+				$issue_title = '';
+				$issue_description= '';
+			} else {
+				$query = "SELECT Title, Description FROM `%table` WHERE IssueId = :id";
+				$values = array('%table' => TABLE_ISSUES, ':id' => $issue_id);
+				$stmt = db()->preparedStatement($query, $values);
+				if ($stmt->foundRows == 1) {
+					$issue = $stmt->fetchObject();
+					$issue_title = $issue->Title;
+					$issue_description = $issue->Description;
+				} else {
+					die('Issue not found: ' . $issue_id);
+				}
+				$title = 'Edit Issue "' . htmlentities($issue_title) . '"';
+			}
+			$vars = array(
+				'issue_id' => $issue_id,
+				'issue_title' => $issue_title,
+				'issue_description' => $issue_description,
+				'page_url' => $page_url,
+				'title' => $title,
+			);
+			display(APP_TITLE, 'Manage|Issues', 'issues_edit.tpl.php', $vars);
+		break;
+	case 'list':
+			$issues = array();
+			$query = "SELECT IssueId, Title, Description FROM `%table`";
+			$values = array('%table' => TABLE_ISSUES);
+			$stmt = db()->preparedStatement($query, $values);
+			while ($issue = $stmt->fetchObject()) {
+				$issues[$issue->IssueId] = $issue;
+			}
+			$vars = array(
+				'issues' => $issues,
+				'page_url' => $page_url,
+			);
+			display(APP_TITLE, 'Manage|Issues', 'issues_list.tpl.php', $vars);
+		break;
+	default:
+		die('Unknown action: ' . htmlentities($action));
+		break;
+}
---
+++ templates/issues_list.tpl.php
@@ -0,0 +1,51 @@
+<h2>Issues</h2>
+
+<table class="table table-striped">
+<tr>
+  <th>Name</th>
+  <th>Description</th>
+  <th>Actions</th>
+</tr>
+<?php foreach ($issues as $id => $data) { ?>
+  <tr>
+	<td><?php print(htmlentities($data->Title)); ?></td>
+	<td><?php print(htmlentities($data->Description)); ?></td>
+	<td>
+		<button class="btn btn-default" onclick="edit_issue('<?php print(htmlentities($id)); ?>')">
+			<span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Edit
+		</button>
+		<button class="btn btn-danger" onclick="delete_issue('<?php print(htmlentities($id)); ?>', '<?php print(htmlentities($data->Title)); ?>')">
+			<span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Delete
+		</button>
+	</td>
+  </tr>
+<?php } ?>
+</table>
+<button class="btn btn-primary" onclick="edit_issue('<?php print(htmlentities(NEW_ENTRY_ID)); ?>')">
+	<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> New Issue
+</button>
+
+<script type="text/javascript">
+function edit_issue(issue_id) {
+	document.location = "<?php print(htmlentities($page_url)); ?>?action=edit&id=" + issue_id;
+}
+function delete_issue(issue_id, issue_title) {
+	bootbox.dialog({
+		message: "Do you want to delete issue \"" + issue_title + "\"",
+		title: "<?php print(htmlentities(APP_TITLE)); ?>",
+		buttons: {
+			cancel: {
+				label: "Don't Delete",
+				className: "btn-default",			
+			},
+			delete: {
+				label: "Delete",
+				className: "btn-danger",
+				callback: function() {
+					document.location = "<?php print(htmlentities($page_url)); ?>?action=delete&id=" + issue_id;
+				}
+			}
+		}
+	});
+}
+</script>
