b6be4a23ab641ead2866a4ae7ef73ce4a58a1086
Tobias Tiederle <ttiederle@fimltd.org>
2015-07-14 12:58:57 +0100

    handle frontpage issue change gracefully

--- js/liveticker.js
+++ js/liveticker.js
@@ -12,8 +12,11 @@ var issues = {};
 
 var poll_timer = null;
 function poll_issue() {
+	if (paused) {
+		return;
+	}
 	console.debug('poll issue ' + issue_id);
-			
+
 	var data = {
 		action: 'get_statements',
 		issue: issue_id,
@@ -31,18 +34,29 @@ function poll_issue() {
 			if (reply['success']) {
 				console.info('Update success: ' + reply['message']);
 				var first_update = (max_statement_id == 0);
+
+				if (first_update) {
+					// clear out div
+					var scroller = $( '#ticker-scroller' );
+					scroller.slideUp(animate_normal);
+					scroller.find( '.ticker-statement' ).each(function( index ) {
+						$( this ).remove();
+					});
+					scroller.slideDown(animate_fast);
+				}
+
 				$.each(reply['data'], function( index, value ) {
-					
+
 					var statement_id = parseInt(index);
 					var statement_text = value['Statement'];
 					var statement_issue_id = value['IssueId'];
-					
+
 					if (statement_id > max_statement_id) {
 						max_statement_id = statement_id;
 					}
 					console.debug(statement_id.toString() + " - " + statement_text);
 					var statement = $("<div class='ticker-statement' id='statement-" + statement_id.toString() +"'></div>").text(statement_text);
-					
+
 					if (issue_id == 0) {
 						var issue = $("<br><span class='issue'></span>");
 						issue.text(issues[statement_issue_id]);
@@ -59,24 +73,29 @@ function poll_issue() {
 			}
 		}
 	});
-	
+
 	poll_timer = setTimeout(function() { poll_issue(); }, poll_interval);
 }
 
 function insert_divider() {
 	var first_child = $( '#ticker-scroller' ).children().first();
-	console.log(first_child);
-	if (first_child.hasClass('ticker-divider')) {
-	} else {
+	if (! first_child.hasClass('ticker-divider')) {
 		$('#ticker-scroller').prepend('<hr class="ticker-divider">');
 	}
 }
 
 function reload_screen() {
+	// pause this
+	paused = true;
 	clearTimeout(poll_timer);
+	$('#pauseButton').html('<span class="glyphicon glyphicon-play"></span>');
+
+	// this might be an issue problem
+	get_issues();
+
 	bootbox.dialog({
 		title: "Error updating data",
-		message: "<p>An error occurred trying to update some data.</p><p>This page should be reloaded</p>",
+		message: "<p>An error occurred trying to update some data.</p><p>For example the Issue might no longer be on the Frontpage.</p><p>You can Reload the page or Cancel to keep the current page frozen</p>",
 		buttons: {
 			cancel: {
 				label: "Cancel",
@@ -107,7 +126,25 @@ function get_issues() {
 		success: function(reply, message) {
 			if (reply['success']) {
 				console.info('Retrieved issues: ' + reply['message']);
+
 				issues = reply['data'];
+
+				// update select
+				var select = $('#issue_id');
+				select.empty();
+				select.append('<option value="0">All Frontpage Issues</option>');
+				$.each(issues, function( index, value ) {
+					$('<option value="' + index + '"></option>').text(value).appendTo(select);
+				});
+
+				// check if old issue_id still exists
+				if (issue_id in issues) {
+					$( '#issue_id' ).val(issue_id);
+				} else {
+					$( '#issue_id' ).val(0);
+					issue_id = 0;
+					max_statement_id = 0;
+				}
 			} else {
 				console.error('Get issues failure: ' + reply['message']);
 				reload_screen();
@@ -122,46 +159,41 @@ function update_title() {
 	} else {
 		$( '#text-issue-title' ).text(' - ' + issues[issue_id]);
 	}
-	
-	// sometimes the select value is not in sync...
-	$( '#issue_id' ).val(issue_id);
 }
 
 $( document ).ready(function() {
-	$.when(get_issues()).done(function() {		
-		poll_issue();
+	$.when(get_issues()).done(function() {
 		update_title();
+		poll_issue();
 	});
 	$( '#issue_id' ).change(function() {
 			// stop updates
 			clearTimeout(poll_timer);
-			
+
 			// set new start
 			issue_id = $( '#issue_id' ).val();
 			max_statement_id = 0;
-			
-			// clear out div
-			var scroller = $( '#ticker-scroller' );
-			scroller.slideUp(animate_normal);
-			scroller.find( '.ticker-statement' ).each(function( index ) {
-				$( this ).remove();
-			});
-			scroller.slideDown(animate_fast);
-			
+
 			// start polling again
-			poll_issue();
 			update_title();
+			poll_issue();
 	});
 	$('#pauseButton').click(function () {
+		paused = ! paused;
 		if (paused) {
-			insert_divider();
-			poll_issue();			
-			$(this).html('<span class="glyphicon glyphicon-pause"></span>');
-			paused = false;
-		} else {
 			clearTimeout(poll_timer);
 			$(this).html('<span class="glyphicon glyphicon-play"></span>');
-			paused = true;
+		} else {
+			// if this was paused due to an error, we need to update the title
+			update_title();
+			
+			// insert divider to make sure new statements stand out
+			insert_divider();
+			
+			// start polling
+			poll_issue();
+
+			$(this).html('<span class="glyphicon glyphicon-pause"></span>');
 		}
 	});
 });
--- templates/liveticker.tpl.php
+++ templates/liveticker.tpl.php
@@ -4,10 +4,6 @@
 	<form class="form-inline">
 		<div class="form-group">
 			<select id="issue_id">
-				<option value="0" <?php if ($issue_id == 0) { print("selected"); } ?>>All Frontpage Issues</option>
-				<?php foreach ($issues as $id => $value) { ?>
-					<option value="<?php print($id); ?>" <?php if ($issue_id == $id) { print("selected"); } ?>><?php print(htmlentities($value)); ?></option>
-				<?php } ?>
 			</select>
 		</div>
 		<div id="ticker-buttons" class="form-group">
@@ -28,7 +24,6 @@
 <?php if (DEBUG) { ?>
 <div class="hidden-print">
 	<h2>Debug</h2>
-	<pre>$summaries = <?php print_r($summaries); ?></pre>
-	<pre>$statements = <?php print_r($statements); ?></pre>
+	<pre>$issues = <?php print_r($issues); ?></pre>
 </div>
 <?php } ?>
